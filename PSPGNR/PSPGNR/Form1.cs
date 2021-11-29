bf02d54da3753eb7e3442b8e0b94f948644447e7
RicardoSampaio12121 <ricardo_cs@outlook.pt>
2021-11-29 22:47:36 +0000

    Started to implement the system to get and XML and a JSON file and deserialize it into a database.
    Can already read a XML file and serialize it to an object.

---
+++ PSPGNR/PSPGNR/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace PSPGNR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+            XmlSerializer serializer = new XmlSerializer(typeof(Fiscalizacao));
+            Fiscalizacao utentes = null;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        utentes = (Fiscalizacao)serializer.Deserialize(reader);
+                    }
+                }
+            }
+            
+            if (utentes != null)
+            {
+                foreach (var u in utentes.Utente)
+                {
+                    MessageBox.Show($"CC: {u.Nus}  |  FN: {u.Firstname}  |  LN: {u.Lastname}");
+                }
+            }
+        }
+    }
+}
---
+++ PSPGNR/PSPGNR/Utente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace PSPGNR
+{
+    [XmlRoot(ElementName = "utente")]
+    public class Utente
+    {
+        [XmlElement(ElementName = "nus")]
+        public string Nus { get; set; }
+        [XmlElement(ElementName = "first-name")]
+        public string Firstname { get; set; }
+        [XmlElement(ElementName = "last-name")]
+        public string Lastname { get; set; }
+        [XmlElement(ElementName = "infracao")]
+        public string Infracao { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Fiscalizacao")]
+    public class Fiscalizacao
+    {
+        [XmlElement(ElementName = "utente")]
+        public List<Utente> Utente { get; set; }
+    }
+}
